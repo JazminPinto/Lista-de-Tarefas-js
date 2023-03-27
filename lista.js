6812fae345e99adf1ca3303d11a847133dead25a
jazmin pinto <jazminpinto61@gmail.com>
2023-03-26 21:01:34 -0300

    ajustando o modal

--- lista.js
+++ lista.js
@@ -1,9 +1,19 @@
 let inputNovaTarefa = document.querySelector('#inputNovaTarefa');
 let btnAddTarefa = document.querySelector('#btnAddTarefa');
 let listaTarefas = document.querySelector('#listaTarefas');
+let janelaEdicao = document.querySelector('#janelaEdicao');
+let janelaEdicaoFundo = document.querySelector('#janelaEdicaoFundo');
+let janelaEdicaoBtnFechar = document.querySelector('#janelaEdicaoBtnFechar');
+let btnAtualizarTarefa = document.querySelector('#btnAtualizarTarefa');
+let idTarefaEdicao = document.querySelector('#idTarefaEdicao');
+let inputTarefaNomeEdicao = document.querySelector('#inputTarefaNomeEdicao');
+
+function gerarId() {
+    return Math.floor(Math.random() * 3000)
+}
 
 inputNovaTarefa.addEventListener('keypress', (e) /*para saber si una tecla esta siendo presionada*/ => {
-    if(e.keyCode == 13) /*o numero do enter e 13 pode verificar no keyCode.inf*/ {
+    if(e.keyCode == 13) /*o numero do enter e 13 pode verificar no keyCode.info*/ {
         let tarefa = {
             nome: inputNovaTarefa.value,
             id: gerarId(),
@@ -12,18 +22,41 @@ inputNovaTarefa.addEventListener('keypress', (e) /*para saber si una tecla esta 
     }
 });
 
+janelaEdicaoBtnFechar.addEventListener('click', (e) => {
+    alternarJanelaEdicao();
+})
+
 btnAddTarefa.addEventListener('click' , (e) =>{
-   
+    
     let tarefa = {
         nome: inputNovaTarefa.value,
         id: gerarId(),
     }
     adicionarTarefa(tarefa);
-})
+});
 
-function gerarId() {
-    return Math.floor(Math.random() * 3000)
-}
+
+btnAtualizarTarefa.addEventListener('click', (e) => {
+    e.preventDefault();
+    
+    let idTarefa = idTarefaEdicao.innerHTML.replace('#', '');
+
+    let tarefa ={
+        nome: inputTarefaNomeEdicao.value,
+        id: idTarefa
+    }
+    
+    let tarefaActual = document.getElementById(''+idTarefa+'');
+
+    if(tarefaActual){
+        let li = criarTagLI(tarefa);
+        listaTarefas.replaceChild(li, tarefaActual);
+        alternarJanelaEdicao();
+    }else{
+        alert('elemento HTML nao encontrado');
+    }
+});
+
 
 function adicionarTarefa(tarefa) {
    
@@ -35,6 +68,7 @@ function adicionarTarefa(tarefa) {
 function criarTagLI(tarefa){
    
     let li = document.createElement('li');
+    li.id = tarefa.id;
 
     let span = document.createElement('span');
     span.classList.add('textoTarefa');
@@ -61,9 +95,30 @@ function criarTagLI(tarefa){
 }
 
 function editar(idTarefa) {
-    alert(idTarefa)
+
+    let li = document.getElementById(idTarefa);
+    if(li) {
+        idTarefaEdicao.innerHTML = '#' + idTarefa;
+        inputTarefaNomeEdicao.value = li.innerText;
+        alternarJanelaEdicao()
+    }else{
+        alert('elemento HTML nao encontrado');
+    }
 }
 
 function excluir(idTarefa) {
-    alert(idTarefa)
+    let confirmacao = window.confirm('Tem certeza que deseja excluir?' );   
+    if(confirmacao){
+        let li = document.getElementById(''+ idTarefa + '');
+        if(li){
+            listaTarefas.removeChild(li);
+        }else{
+            alert('elemento HTML nao encontrado');
+        }
+    }
+}
+
+function alternarJanelaEdicao(){
+    janelaEdicao.classList.toggle('abrir');
+    janelaEdicaoFundo.classList.toggle('abrir');
 }
